6e7a09e00b6492359d4ce34baed3f7357a8147c3
luis chagas <luischagas78@gmail.com>
2018-11-07 11:28:34 -0500

    backend for profile and matches pages done

--- db/seeds.rb
+++ db/seeds.rb
@@ -102,90 +102,26 @@ sundar = User.create!(
 
 puts "Creating questions..."
 
-QUESTIONS = [
-  {
-  question: "Are you looking for a flatmate?", # question_id = 0
-  display: "I'm looking for a flatmate"
-  },
-  {
-  question: "Do you have a house to share?", # question_id = 1
-  display: "I have a house to share"
-  },
-  {
-  question: "Are you looking for a parking spot to rent?", # question_id = 2
-  display: "I'm looking for a parking spot to rent"
-  },
-  {
-  question: "Do you have a parking spot to rent?", # question_id = 3
-  display: "I have a parking spot to rent"
-  },
-  {
-  question: "Do you feel like talking about Red dead redemption?", # question_id = 4
-  display: "I feel like talking about Red dead redemption"
-  },
-  {
-  question: "Do you feel like talking about Artificial Intelligence?", # question_id = 5
-  display: "I feel like talking about Artificial Intelligence"
-  },
-  {
-  question: "Do you feel like talking about Black Mirror?", # question_id = 6
-  display: "I feel like talking about Black Mirror"
-  },
-  {
-  question: "Do you feel like talking about The Simpsons?", # question_id = 7
-  display: "I feel like talking about The Simpsons"
-  },
-  {
-  question: "Do you feel like talking about Cars", # question_id = 8
-  display: "I feel like talking about Cars"
-  },
-  {
-  question: "Are you looking for someone to play PlayStation 4 with?", # question_id = 9
-  display: "I'm looking for someone to play PlayStation 4 with"
-  },
-  {
-  question: "Are you looking for someone to go dancing with?", # question_id = 10
-  display: "I'm looking for someone to go dancing with"
-  },
-  {
-  question: "Are you looking for someone to do sports with?", # question_id = 11
-  display: "I'm looking for someone to do sports with"
-  },
-  {
-  question: "Are you looking for someone to go to the gym with?", # question_id = 12
-  display: "I'm looking for someone to go to the gym with"
-  },
-  {
-  question: "Are you looking for someone to go running with?", # question_id = 13
-  display: "I'm looking for someone to go running with"
-  },
-  {
-  question: "Are you looking for an investor for a project you are working on?", # question_id = 14
-  display: "I'm looking for an investor for a project you are working on"
-  },
-  {
-  question: "Are you looking for a business partner?", # question_id = 15
-  display: "I'm looking for a business partner"
-  },
-]
 
 # INTERESTS ----------------------------
 
 puts "Creating interests..."
 
-50.times do
-  random = (0...QUESTIONS.size).to_a.sample
-  Interest.create!(
-    user: User.all.sample,
-    question_id: random,
-    answer: QUESTIONS[random][:display]
-  )
+User.all.each do |user|
+  5.times do
+    random = (0...Interest::QUESTIONS.size).to_a.sample
+    Interest.create!(
+      user: user,
+      question_id: random,
+      answer: "true",
+    )
+  end
 end
 
 
 # MATCHES -----------------------------
 
-puts "Creating matches"
+puts "Creating matches..."
 
 Match.create!(
     from_user: marco,
