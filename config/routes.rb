9775f70704a832ff8576e27463fbbeafd0cc427d
Angelo Callari <angelo280@hotmail.it>
2018-11-07 16:51:52 +0100

    home skeleton made

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :pages
-  get "/profile", to: 'pages#profile'
+  get '/pages', to: 'pages#index'
+  get '/profile', to: 'pages#profile'
+  get '/matches', to: 'pages#matches'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
