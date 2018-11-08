0c4164644394d0a17e936ee0377220acaf842bfc
Angelo Callari <angelo280@hotmail.it>
2018-11-08 14:56:56 +0100

    display avatar now works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_up_path_for(resource)
+    profile_path
+  end
 
     protected
 
