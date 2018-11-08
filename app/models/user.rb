d0286dca857eaf45787d4dc513a7023930347893
Angelo Callari <angelo280@hotmail.it>
2018-11-08 12:28:47 +0100

    modifing user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,19 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  mount_uploader :photo, PhotoUploader
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :remember_me, :avatar, :avatar_cache, :remove_avatar
 
 
   has_many :from_user_matches, class_name: 'Match', foreign_key: 'from_user_id'
   has_many :to_user_matches, class_name: 'Match', foreign_key: 'to_user_id'
   has_many :interests
+
 
   def valid_interest
     self.interests.where(answer: "true")
