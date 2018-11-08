4356358369e80b05b40490f2993a67dfefa2a257
Angelo Callari <angelo280@hotmail.it>
2018-11-08 12:39:19 +0100

    now i can see something in the photo field

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,16 +6,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :remember_me, :avatar, :avatar_cache, :remove_avatar
-
-
   has_many :from_user_matches, class_name: 'Match', foreign_key: 'from_user_id'
   has_many :to_user_matches, class_name: 'Match', foreign_key: 'to_user_id'
   has_many :interests
 
 
+
   def valid_interest
     self.interests.where(answer: "true")
   end
+
+  # validates_presence_of   :photo
+  # validates_integrity_of  :photo
+  # validates_processing_of :photo
+
 end
