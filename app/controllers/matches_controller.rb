b297c763cab467995e6128c48652ecc43aeb6382
Angelo Callari <angelo280@hotmail.it>
2018-11-12 16:24:55 +0100

    now all sendin message stuff works

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -15,7 +15,8 @@ class MatchesController < ApplicationController
   def update
     @m = Match.where(from_user: User.find(params[:match][:from_user_number]), to_user: current_user).first
     @m.status = params[:status] if params[:status]
-    @m.photo = params[:match][:photo] if params[:match]
+    @m.photo = params[:match][:photo] if params[:match][:photo]
+    @m.instructions = params[:match][:instructions] if params[:match][:instructions]
     @m.save!
     if @m.accepted?
       redirect_to profile_path(@m.from_user_id)
@@ -25,6 +26,6 @@ class MatchesController < ApplicationController
   end
 
   def match_params
-    params.require(:match).permit(:status, :photo)
+    params.require(:match).permit(:status, :photo, :instructions)
   end
 end
