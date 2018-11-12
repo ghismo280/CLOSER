390d48b5aa5fc58073ee5bf722ceb0711f18ba5d
Angelo Callari <angelo280@hotmail.it>
2018-11-12 15:10:03 +0100

    now show 400 x 400 images in the index

--- db/migrate/20181112135401_add_imgmessage_to_matches.rb
+++ db/migrate/20181112135401_add_imgmessage_to_matches.rb
@@ -1,5 +1,5 @@
 class AddImgmessageToMatches < ActiveRecord::Migration[5.2]
   def change
-    add_column :matches, :photo, :string
+    add_column :matches, :imgmessage, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_12_114050) do
+ActiveRecord::Schema.define(version: 2018_11_12_135401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2018_11_12_114050) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
+    t.string "imgmessage"
     t.index ["from_user_id"], name: "index_matches_on_from_user_id"
     t.index ["to_user_id"], name: "index_matches_on_to_user_id"
   end
