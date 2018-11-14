820577361eede0ed0413397f8596120d1b1da19b
Elvia Garcia <elviagarciaro@gmail.com>
2018-11-14 12:48:27 +0100

    patch from Damian

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,12 @@
 //= require rails-ujs
 //= require_tree .
 
-navigator.geolocation.getCurrentPosition(function(position) {
-  document.getElementById('user_latitude').value = position.coords.latitude;
-  document.getElementById('user_longitude').value = position.coords.longitude;
+if (document.getElementById('user_latitude')) {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    document.getElementById('user_latitude').value = position.coords.latitude;
+    document.getElementById('user_longitude').value = position.coords.longitude;
 
-  // alert("here the geoloc!")
-  document.getElementById("button_signup_user").disabled = false;
-});
+    // alert("here the geoloc!")
+    document.getElementById("button_signup_user").disabled = false;
+  });
+}
